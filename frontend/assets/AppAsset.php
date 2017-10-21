f8a9d246099f2234199b61deadca769e6e94f6d1
appdev1 <m.yunus@duakelinci.co.id>
2017-10-22 04:08:58 +0700

    include font awesome

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -13,6 +13,7 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        '../../backend/web/css/font-awesome-4.7.0/css/font-awesome.min.css'
     ];
     public $js = [
         'js/main.js',
